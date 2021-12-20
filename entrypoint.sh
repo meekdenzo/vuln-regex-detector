0d5956c6b08ecfe28d1a2ee1ed8b01ff6bb4e21e
Parallels <meekdenzo@gmail.com>
2021-12-19 23:32:17 -0600

    test: bash script for action

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Install dependencies
+echo "Installing dependencies..."
+apt install -yq curl jq
+
+# Run configurations
+export VULN_REGEX_DETECTOR_ROOT=`pwd`
+./configure
+
+# Scan for redos
+changed_files=`git diff --name-only`
+for i in ${changed_files}
+    do
+        echo "Scanning for vulnerable regexes in $i"
+        echo '{"file":"'$i'"}' > repo.json   
+        
+        perl ./bin/check-file.pl repo.json > repo-out.json
+
+        echo "The following vulnerable regexes were found in $i"
+        cat repo-out.json | jq -r '.vulnRegexes | .[]?'
+        printf "\n\n\n\n"
+    done
+
+# Suggest changes
