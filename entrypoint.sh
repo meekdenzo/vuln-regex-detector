c13e935fed8ec3ea284cece53aee7926eec8a1b9
Parallels <meekdenzo@gmail.com>
2021-12-30 19:17:30 -0600

    test: workflow from @Trott

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,40 +3,35 @@
 set -e
 
 # Install dependencies
-echo "Installing dependencies..."
-sudo apt install -yq curl jq
+echo "Checking/installing dependencies..."
+command -v curl || sudo apt-get install -yq curl
+command -v jq || sudo apt-get install -yq jq
 
 # Run configurations
-SECONDS_=0
-export VULN_REGEX_DETECTOR_ROOT=`pwd`
-./configure &>/dev/null
-duration=$SECONDS_;
-echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed for configuration."
+VULN_REGEX_DETECTOR_ROOT=$(pwd)
+export VULN_REGEX_DETECTOR_ROOT
+./configure
 
 echo 'Configuration complete'
 
 # test
 echo '{"file":"./autoInject.js"}' > repo.json   
 perl ./bin/check-file.pl repo.json > repo-out.json
-cat repo-out.json | jq -r '.vulnRegexes | .[]?'
+jq -r '.vulnRegexes | .[]?' < repo-out.json
 # Scan for redos
-changed_files=`git show --name-only --pretty=format:`
-echo $changed_files
+changed_files=$(git diff --name-only "$GITHUB_BASE_REF" "$GITHUB_HEAD_REF")
+echo "$changed_files"
 
-SECONDS=0
 for i in ${changed_files}
     do
         echo "Scanning for vulnerable regexes in $i"
-        echo '{"file":"'$i'"}' > repo.json   
+        echo '{"file":"'"$i"'"}' > repo.json
        
         perl ./bin/check-file.pl repo.json > repo-out.json
 
         echo "The following vulnerable regexes were found in $i"
-        cat repo-out.json | jq -r '.vulnRegexes | .[]?'
+        jq -r '.vulnRegexes | .[]?' < repo-out.json
         printf "\n\n\n\n"
     done
-
-duration=$SECONDS;
-echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed for scan."
 
 # Suggest changes
