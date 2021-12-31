8dd61db31a79b1c5f3c593ae1fb27146a3e11dee
Parallels <meekdenzo@gmail.com>
2021-12-30 18:42:28 -0600

    test: workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ echo '{"file":"./autoInject.js"}' > repo.json
 perl ./bin/check-file.pl repo.json > repo-out.json
 cat repo-out.json | jq -r '.vulnRegexes | .[]?'
 # Scan for redos
-changed_files=`git diff --name-only`
+changed_files=`git show --name-only --pretty=format:`
 echo $changed_files
 
 for i in ${changed_files}
