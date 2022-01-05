097c45f8cb8f5792640004a9566e6d173248901a
Parallels <meekdenzo@gmail.com>
2022-01-05 17:15:05 -0600

    test: clone repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,27 +3,29 @@
 set -e
 
 # Install dependencies
-# echo "Checking/installing dependencies..."
-# command -v curl || sudo apt-get install -yq curl
-# command -v jq || sudo apt-get install -yq jq
+echo "Checking/installing dependencies..."
+command -v curl || sudo apt-get install -yq curl
+command -v jq || sudo apt-get install -yq jq
 
-# # Run configurations
-# VULN_REGEX_DETECTOR_ROOT=$(pwd)
-# export VULN_REGEX_DETECTOR_ROOT
-# ./configure
+CHANGED_FILES=$(git diff --name-only HEAD^ HEAD | grep -v "^src/validate*")
+echo "$CHANGED_FILES"
 
-# echo 'Configuration complete'
+# Run configurations
+git clone https://github.com/davisjam/vuln-regex-detector.git
+cd vuln-regex-detector
+VULN_REGEX_DETECTOR_ROOT=$(pwd)
+export VULN_REGEX_DETECTOR_ROOT
+./configure
+cd ..
 
 # test
 echo '{"file":"./autoInject.js"}' > checkfile.json   
 perl ./bin/check-file.pl checkfile.json > checkfile-out.json
 jq -r '.vulnRegexes | .[]?' < checkfile-out.json
 # Scan for redos
-changed_files=$(git diff --name-only HEAD^ HEAD | grep -v "^src/validate*")
-echo "$changed_files"
 
 SECONDS=0
-for i in ${changed_files}
+for i in ${CHANGED_FILES}
     do
         echo "Scanning for vulnerable regexes in $i"
         echo '{"file":"'"$i"'"}' > checkfile.json
